731ba61eba9d7b8d578ce62aaec99e7f55a6c6a6
Sebastian Stamm <sebastian.stamm@camunda.com>
2021-04-26 15:06:47 +0200

    make nightmode smaller and live-coding friendly

--- nightmode/src/index.js
+++ nightmode/src/index.js
@@ -6,23 +6,8 @@ export default {
   priority: 9002,
   render: (container) => {
     container.innerHTML = "<button>🌙</button>";
-    container.style.position = "absolute";
-    container.style.right = "155px";
-    container.style.top = "6px";
 
-    const btn = container.querySelector("button");
-    btn.style.borderRadius = "50%";
-    btn.style.width = "30px";
-    btn.style.height = "30px";
-    btn.style.fontSize = "17px";
-    btn.style.borderColor = "transparent";
-    btn.style.padding = "0";
-    btn.style.textAlign = "center";
-    btn.style.backgroundColor = "#333";
-
-    document.body.style.transition = "filter 0.5s";
-
-    btn.addEventListener("click", () => {
+    container.querySelector("button").addEventListener("click", () => {
       active = !active;
 
       if (active) {
